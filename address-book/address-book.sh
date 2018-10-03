2746e9ba3442a5a24adcf4fd86422f86bf243a56
Hunter D Poe <poe.hunter@gmail.com>
2018-10-02 23:57:44 -0600

    Adds some test data and list ability for the directory

--- address-book/address-book.sh
+++ address-book/address-book.sh
@@ -3,13 +3,13 @@
 function main {
   declare -l ADD_BOOK_F 
 
-  ADD_BOOK_F=${1-address-book.txt}
-
+#  ADD_BOOK_F=${1-address-book.txt}
+  ADD_BOOK_F="address-book.txt"
   if [ ! -e $ADD_BOOK_F ] 
   then touch $ADD_BOOK_F
   fi
 
-  case $2 in
+  case $1 in
     'add')
       # Add record here
       ;;
@@ -22,21 +22,44 @@ function main {
     'mod')
       # Update record here
       ;;
+    'list')
+      listAddressBook $ADD_BOOK_F
+      ;;
     *)
-      invalidMenuSelect $2
+      invalidMenuSelect $1
       ;;
   esac
 }
 
+function listAddressBook {
+  declare -a addresses
+  lines=($(cat $1))
+  for line in ${lines[@]}
+  do
+   declare -A record;
+   line=($(echo ${line} | sed 's/,/\t/g'))
+   record[sName]=${line[0]}
+   record[fName]=${line[1]}
+   record[telephoneNum]=${line[2]}
+   record[address]=${line[3]}
+   echo -e "Name: ${record[sName]}, ${record[fName]}\nTelephone Number: ${record[telephoneNum]}\nAddress: ${record[address]}"
+   echo "======="
+  done
+}
+
 function invalidMenuSelect {
-  echo "Unknown command: $1
-  Valid commands are 
+  echo "Unknown command: ${1}"
+  displayHelp
+}
+
+function displayHelp {
+  echo "Valid commands are 
   add: To add record
   find: To find record
   del: To delete record
   mod: To modify a record
-  help: To display this menu again
-  "
+  list: To list a record
+  help: To display this menu again"
 }
 
 main $@
